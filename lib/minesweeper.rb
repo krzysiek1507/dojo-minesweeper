c0f63c4423da03c6715c36d926b38cfd43861f93
Konrad Szczęśniak <konrad.szczesniak@elpassion.pl>
2014-08-28 12:40:16 +0200

    after 1:40h of code dojo

--- lib/minesweeper.rb
+++ lib/minesweeper.rb
@@ -1,2 +1,104 @@
 class Minesweeper
+  def initialize(input)
+    @input = input
+  end
+
+  def generate
+    @board = empty_board(number_of_rows,number_of_columns)
+    put_bombs
+    put_numbers
+    MinesweeperBoardPrinter.print(@board)
+  end
+
+  private
+
+  def put_bombs
+    number_of_rows.times do |row|
+      number_of_columns.times do |column|
+        @board.board[row][column] = '*' if contains_bomb?(row, column)
+      end
+    end
+  end
+
+  def put_numbers
+    number_of_rows.times do |row|
+      number_of_columns.times do |column|
+        @board.board[row][column] = count_surrounding_bombs(row, column).to_s unless contains_bomb?(row, column)
+      end
+    end
+  end
+
+  def count_surrounding_bombs(row, column)
+    if contains_bomb?(row, column)
+      0
+    else
+      bombs = 0
+      (row-1..row+1).each do |r|
+        (column-1..column+1).each do |c|
+          bombs += 1 if contains_bomb?(r, c)
+        end
+      end
+      bombs
+    end
+  end
+
+  def empty_board?
+    @input == '0 0'
+  end
+
+  def contains_bomb?(row, column)
+    field(row, column) == '*'
+  end
+
+  def field(row, column)
+    if row<0 || row>number_of_rows-1 || column<0 || column>number_of_columns-1
+      '.'
+    else
+      @input.lines[row + 1][column]
+    end
+  end
+
+  def number_of_rows
+    @input[0].to_i
+  end
+
+  def number_of_columns
+    @input[2].to_i
+  end
+
+  def empty_board(rows, colums)
+    MinesweeperBoard.new(rows,colums)
+  end
+end
+
+class MinesweeperBoard
+  attr_accessor :board
+  attr_reader :rows, :columns
+
+  def initialize(rows, columns)
+    @rows = rows
+    @columns = columns
+    @board = []
+
+    @rows.times do |row|
+      @board[row] = []
+      @columns.times do |column|
+        @board[row][column] = '0'
+      end
+    end
+  end
+end
+
+class MinesweeperBoardPrinter
+
+  def self.print(board)
+    string = ''
+    board.rows.times do |row|
+      board.columns.times do |column|
+        string << board.board[row][column]
+      end
+      string << "\n"
+    end
+    string.chop
+  end
 end
--- spec/lib/minesweeper_spec.rb
+++ spec/lib/minesweeper_spec.rb
@@ -1,6 +1,40 @@
 require_relative '../../lib/minesweeper'
 
 describe Minesweeper do
-  it { is_expected.not_to be_nil }
+  subject { Minesweeper.new(input)}
 
+  context 'empty board' do
+    let (:input) { '0 0' }
+
+    it 'should return empty string' do
+      expect(subject.generate).to eq("")
+    end
+  end
+
+  context '1x1 board' do
+    context 'with bomb' do
+      let (:input) { "1 1\n*" }
+
+      it { expect(subject.generate).to eq('*') }
+    end
+
+    context 'without bomb' do
+      let (:input) { "1 1\n." }
+
+      it { expect(subject.generate).to eq('0') }
+    end
+  end
+
+  context '2x1 board' do
+    context 'without bomb' do
+      let (:input) { "2 1\n.\n."}
+
+      it {expect(subject.generate).to eq("0\n0")}
+    end
+    context 'with bomb' do
+      let (:input) { "2 1\n*\n."}
+
+      it {expect(subject.generate).to eq("*\n1")}
+    end
+  end
 end
